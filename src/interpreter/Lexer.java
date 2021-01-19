60a6dabef5f5e6dfd8b5dc01651d603777d3e12a
omerharush93 <63789544+omerharush93@users.noreply.github.com>
2021-01-19 20:28:52 +0200

    Delete src directory

--- src/interpreter/Lexer.java
+++
@@ -1,6 +0,0 @@
-package interpreter;
-import java.util.ArrayList;
-
-public interface Lexer<V> {
-	public ArrayList<String[]> lexicalCheck();
-}
--- src/interpreter/Parser.java
+++
@@ -1,5 +0,0 @@
-package interpreter;
-
-public interface Parser {
-    public void parse();
-}
